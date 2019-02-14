250a47aa46993d5c1de9f588425ab17cd8deafe2
Tulip Pink <pinktulips@hotmail.com>
2019-02-14 13:08:17 -0800

    IPost interface

---
+++ NET-Gram/NET-Gram/Models/Interfaces/IPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_Gram.Models.Interfaces
+{
+    public interface IPost
+    {
+
+        //Delete
+        Task DeleteAsync(int id);
+
+        //Find
+        Task<Post> FindPost(int id);
+
+        //GetAll
+        Task<List<Post>> GetPosts();      
+
+        //Save
+        Task SaveAsync(Post post);
+
+      
+    }
+}
