9042985c050e8694f457de47486a7484227b6fbb
Tulip Pink <pinktulips@hotmail.com>
2019-02-14 12:57:33 -0800

    initial commit

--- NET-Gram/NET-Gram/Program.cs
+++ NET-Gram/NET-Gram/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication1
+namespace NET_Gram
 {
     public class Program
     {
