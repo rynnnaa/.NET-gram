0ff667307d01840fb6b06eaf38e11fe9fa1f81c0
Tulip Pink <pinktulips@hotmail.com>
2019-02-14 13:21:33 -0800

    data seeding

--- NET-Gram/NET-Gram/Data/PostDbContext.cs
+++ NET-Gram/NET-Gram/Data/PostDbContext.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NET_Gram.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NET_Gram.Data
+{
+    public class PostDbContext : DbContext
+    {
+        public PostDbContext(DbContextOptions<PostDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(
+               new Post
+               {
+                   ID = 1,
+                   Title = "flowers",
+                   Caption = "In a field of dreams",
+                   Rating = 4,
+                   URL = "flowers.jpg"
+               }
+                );
+        }
+
+        internal void Remove(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        //db table
+        public DbSet<Post> Posts { get; set; }
+    }
+}
