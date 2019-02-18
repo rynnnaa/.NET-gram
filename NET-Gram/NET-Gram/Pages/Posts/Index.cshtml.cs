4933a7f988d0ca4f83d417b7f4adf13856db5585
Tulip Pink <pinktulips@hotmail.com>
2019-02-18 10:41:45 -0800

    Posts folder

---
+++ NET-Gram/NET-Gram/Pages/Posts/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NET_Gram.Pages.Posts
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
