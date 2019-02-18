316a28024f1b6cfbde6a1ccb9cc07e60f861b640
Tulip Pink <pinktulips@hotmail.com>
2019-02-18 13:16:42 -0800

    blob class

---
+++ NET-Gram/NET-Gram/Models/Util/Blob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_Gram.Models.Util
+{
+    public class Blob
+    {
+        public CloudStorageAccount CloudStorageAccount { get; set; }
+        public CloudBlobClient CloudBlobClient { get; set; }
+
+        public Blob(IConfiguration configuration)
+        {
+            CloudStorageAccount = CloudStorageAccount.Parse(configuration["BlobConnectionString"]);
+            CloudBlobClient = CloudStorageAccount.CreateCloudBlobClient();
+        }
+    }
+}
